a7cb38dfae8fb17cc0b4e67dff1439fc44b9e8e0
Dmitry <dimaruzaev2@gmai.com>
2024-09-22 18:42:51 +0300

    ref

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+import fs from "fs";
 
 export const main = (io) => {
 	const onConnection = (socket) => {
-		console.log(1);
-		// audioSocketHandler(socket);
+		socket.on('login-data:send', (data) => {
+			const usersRaw = fs.readFileSync('./src/databases/users.json').toString();
+			const users = JSON.parse(usersRaw);
+			let isValid = false;
+			if (users[data.name] && users[data.name].password === data.password) {
+				isValid = true;
+			}
+			socket.emit('is-valid-login-data:request', isValid);
+			console.log(data, isValid);
+		});
 	};
 
 	io.on("connection", onConnection);
