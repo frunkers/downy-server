011071674cdf425dbe6dab145d56a6e7f3caa4af
Dmitry <dimaruzaev2@gmai.com>
2024-09-22 18:47:07 +0300

    ref

---
+++ src/server-prod.js
@@ -0,0 +1,24 @@
+import { readFileSync } from "fs";
+import { createServer } from "https";
+import { Server } from "socket.io";
+import { main } from "./main.js";
+
+const directory = `/etc/letsencrypt/live/airmonitor.servermc.ru-0001`;
+const ssl = {
+	key: readFileSync(`${directory}/privkey.pem`),
+	cert: readFileSync(`${directory}/fullchain.pem`),
+};
+
+const httpsServer = createServer(ssl);
+const io = new Server(httpsServer, {
+	cors: {
+		origin: ["https://frintest.github.io"],
+	},
+});
+
+main(io);
+
+const PORT = 3003;
+httpsServer.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
